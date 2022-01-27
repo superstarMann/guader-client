e2752728c22b2459fd6bccfc50a2ef7e23efbe82
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2022-01-27 21:31:03 +0900

    finsish create-toggle button

--- guader/src/components/MainMenu.tsx
+++ guader/src/components/MainMenu.tsx
@@ -1,30 +1,59 @@
-import React, { useEffect, useState } from'react'
+import React, { useState } from'react'
 import { gql, useApolloClient, useMutation } from "@apollo/client";
 import { useMe } from './useMe';
-import { useForm } from 'react-hook-form';
 import { TOGGLE_PROTECT_MUTATION } from './SideMenu';
 import { ToggleWalkingMode, ToggleWalkingModeVariables } from '../__generated__/ToggleWalkingMode';
 
 export const MainMenu = () => {
     const client = useApolloClient()
     const {data: userData} = useMe();
-    const [toggleWalkingMode] = useMutation<ToggleWalkingMode, ToggleWalkingModeVariables>(TOGGLE_PROTECT_MUTATION)
+    const onCompleted = () => {
+        console.log(userData?.GetMyProfile.ok)
+    }
     const [isToggle, setIsToggle] = useState(false);
+    const [toggleWalkingMode] = useMutation<ToggleWalkingMode, ToggleWalkingModeVariables>(TOGGLE_PROTECT_MUTATION, {onCompleted})
     const onAClick = (userId: any) => {
+        setIsToggle(!isToggle)
         toggleWalkingMode({
             variables:{
                 userId
             }
         })
-        setIsToggle(!isToggle)
+        if(!isToggle && userData?.GetMyProfile.user){
+            client.writeFragment({
+                id: `User:${userData.GetMyProfile.user.id}`,
+                fragment: gql`
+                fragment StartProtect on User {
+                    isProtecting
+                  }
+                `,
+                data:{
+                    isProtecting: true
+                }
+            })
+        }else if(isToggle && userData?.GetMyProfile.user){
+            client.writeFragment({
+                id: `User:${userData.GetMyProfile.user.id}`,
+                fragment: gql`
+                fragment StopProtect on User{
+                    isProtecting
+                }
+                `,
+                data:{
+                    isProtecting: false
+                }
+            })
+        }
     }
+
+
       return(
           <div>
-              <button onClick={() => onAClick(userData?.GetMyProfile.user?.id)}>{isToggle ? (
+              <button onClick={() => onAClick(userData?.GetMyProfile.user?.id)}>{
+              userData?.GetMyProfile.user?.isProtecting ? (
                 <span>Stop Protecting</span>
               ): (<span>Start Protecting</span>)}</button>
               <span>
-                
               </span>
               </div>
       )
