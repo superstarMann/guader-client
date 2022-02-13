f019998e9ca9a7fdf59bd69b508f9da655e21acf
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2022-02-13 14:46:19 +0900

    delete Button

--- guader/src/components/Place.tsx
+++ guader/src/components/Place.tsx
@@ -1,8 +1,14 @@
-import { useMutation } from '@apollo/client';
-import React, { useState } from 'react';
+import React from 'react';
+import { gql, useApolloClient, useMutation } from '@apollo/client';
 import styled from 'styled-components';
 import { DELETE_PLACE } from '../routes/logIn/Places';
 import { DeletePlace, DeletePlaceVariables } from '../__generated__/DeletePlace';
+
+const Items = styled.div`
+border: 1px solid white;
+padding: 13px 12px;
+border-radius: 5px;
+`
 
 const Item = styled.div`
 display: flex;
@@ -10,45 +16,63 @@ justify-content: space-between;
 margin-bottom: 5px;
 `
 
+const DltBtn = styled.div`
+border: none;
+cursor: pointer;
+font-size: 0.75rem;
+line-height: 1rem;
+`
+
 interface IPlace{
 address: string | undefined; 
 name: string | undefined;
 id?: number | undefined;
 isFav?: boolean;
-dltStart: boolean;
 }
 
-export const Place:React.FC<IPlace> = ({
-    address,
-    name, 
-    id, 
-    dltStart
-}) => {
-    const [deletePlaceMutation] = useMutation<DeletePlace, DeletePlaceVariables>(DELETE_PLACE);
-    const onClick = (id: any) => {
-           
+export const Place:React.FC<IPlace> = ({ address, name, id, isFav }) => {
+    const client = useApolloClient()
+    const onCompleted = (data: DeletePlace) => {
+        const {DeletePlace: {ok, error}} = data;
+        if(ok){
+            console.log('it works')
+        }else if(error){
+            console.log(error)
+        }
     }
+    const [deletePlaceMutation] = useMutation<DeletePlace, DeletePlaceVariables>(DELETE_PLACE,
+        {onCompleted});
+    const onClick = (placeId: any) => {
+        deletePlaceMutation({
+            variables:{
+                placeId
+            }
+        })
+            client.writeFragment({
+                id: `Place:${placeId}`,
+                fragment: gql`
+                fragment DeletePlace on Place{
+                    isFav
+                }
+                `,
+                data:{
+                    isFav: false
+                }
+            })
+    }
+   
     return(
         <>
-        {dltStart ? (
-        <Item>
-            <div onClick={() => onClick(id)}>
-            <Item>
-             Name: {name}
-            <span>{""}</span>
-            </Item>
-            <div>Adderss: {address}</div>
-            </div>
-        </Item>
-            
-        ): (
-            <div onClick={() => onClick(id)}>
-            <Item>
-             Name: {name}
-            <span>{""}</span>
-            </Item>
-            <div>Adderss: {address}</div>
-            </div>
+        {isFav && (
+            <Items>
+                <Item>
+                 Name: {name}
+                    <DltBtn onClick={() => onClick(id)}>
+                     ❌
+                    </DltBtn>
+                </Item>
+                <div>Adderss: {address}</div>
+            </Items>
         )}
         </>
     )
--- guader/src/routes/logIn/Places.tsx
+++ guader/src/routes/logIn/Places.tsx
@@ -7,7 +7,6 @@ import { DashBoard } from '../../components/Dashboard';
 import { GetMyPlaces } from '../../__generated__/GetMyPlaces';
 import { Container} from '../logOut/OutHome';
 import { Place } from '../../components/Place';
-import { DeletePlace, DeletePlaceVariables } from '../../__generated__/DeletePlace';
 
 const Main = styled.div`
 display: flex;
@@ -27,28 +26,6 @@ margin-bottom: 20px;
   margin-bottom: 30px;
  }
 `
-const ToggleBox = styled.div`
-display: flex;
-padding: 7px 20px;
-width: 50%;
-flex-direction: row;
-justify-content: right;
-gap: 10px;
-color: black;
-`
-const ChangeFav = styled.div`
-background-color: #ffeaa7;
-padding: 5px;
-cursor: pointer;
-border-radius: 5px;
-`
-
-const ToggleDelete = styled.div`
-background-color: #ff7675;
-padding: 5px;
-cursor: pointer;
-border-radius: 5px;
-`
 
 const Ul = styled.div`
 width: 50%;
@@ -60,17 +37,6 @@ grid-template-columns: repeat(2, 1fr);
 grid-column-gap: 2em;
 grid-row-gap: 1em;
 `
-const Items = styled(Link)`
-cursor: pointer;
-border: 1px solid white;
-padding: 13px 12px;
-&:hover{
-    border: 1px solid #1abc9c;
-    opacity: 0.7;
-}
-border-radius: 5px;
-`
-
 const AddItems = styled(Link)`
 padding-top: 40px;
 color: #f1c40f;
@@ -105,34 +71,20 @@ mutation DeletePlace($placeId: Int!) {
 
 export const Places = () => {
     const {data}= useQuery<GetMyPlaces>(GET_MY_PLACES);
-    const [dltStart, setDltStart] = useState(false);
-    const triggerBtn = () => {
-        setDltStart(!dltStart);
-    }
-
     return(
         <Container>
             <Helmet><title>{`places | Guader`}</title></Helmet>
             <DashBoard/>
             <Main>
                 <Title>Favorite Places</Title>
-                   <ToggleBox>
-                       {dltStart ? (
-                       <ToggleDelete onClick={triggerBtn}>Done</ToggleDelete>) : (
-                        <ToggleDelete onClick={triggerBtn}>Delete</ToggleDelete>
-                       )}
-                   </ToggleBox>
                 <Ul>
                 {data?.GetMyPlaces.places?.map((place) => (        
-                    <Items to=''>
                       <Place
-                      dltStart={dltStart}
                       id={place?.id}
                       address={place?.address}
                       name={place?.name}
                       isFav={place?.isFav}
                       />
-                    </Items>
                 ))}
                 </Ul>
                 <AddItems to='/add-place'>Please Add your Place! &rarr;</AddItems>
